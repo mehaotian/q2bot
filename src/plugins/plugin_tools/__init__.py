37193fb5856626bb2ddd7588b73efbab74a82c83
mehaotian <490272692@qq.com>
2024-06-20 17:12:42 +0800

    feat: 60s 读世界

--- src/plugins/plugin_tools/__init__.py
+++ src/plugins/plugin_tools/__init__.py
@@ -1,3 +1,4 @@
+import datetime
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import (
     Bot,
@@ -6,6 +7,7 @@ from nonebot.adapters.onebot.v11 import (
 )
 
 moyu = on_command("摸鱼日报", priority=9, block=True)
+lookworld  = on_command("读世界", priority=9, block=True)
 
 
 @moyu.handle()
@@ -17,6 +19,24 @@ async def moyu_handle(bot: Bot, event: GroupMessageEvent):
     """
     url = 'https://api.52vmy.cn/api/wl/moyu'
     # image_file = MessageSegment.image(url)
+    try:
+        image_file = MessageSegment.image(url)
+        await bot.send(event=event, message=image_file)
+
+    except Exception as e:
+        err_msg = f'获取日报失败，错误信息：{e}'
+        await bot.send(event=event, message=err_msg)
+
+@lookworld.handle()
+async def lookworld_handle(bot: Bot, event: GroupMessageEvent):
+    """
+    读世界
+    指令：
+        - /读世界
+    """
+    # 将当前时间转为 20240109 格式
+    time = datetime.datetime.now().strftime('%Y%m%d')
+    url = f'https://jx.iqfk.top/60s/{time}.png'
     try:
         image_file = MessageSegment.image(url)
         await bot.send(event=event, message=image_file)
