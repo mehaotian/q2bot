78f394ba5a074950f57c3dabe74e013831f41466
mehaotian <490272692@qq.com>
2024-09-19 03:00:51 +0800

    feat: 新增小黑盒抽奖

---
+++ src/plugins/nonebot_plugin_group_master/text2img/xhh2img.py
@@ -0,0 +1,357 @@
+from io import BytesIO
+from pathlib import Path
+from PIL import Image, ImageDraw, ImageFont
+from nonebot.log import logger
+from itertools import islice
+from ..config import text_bg_path, static_path
+
+font_path = str(static_path / "KNMaiyuan-Regular.ttf")
+
+
+def adjust_image(image_path, target_width, target_height):
+    """
+    调整图片大小
+    :param image_path: 图片路径
+    :param target_width: 目标宽度
+    :param target_height: 目标高度
+    :return: 调整后的图片
+    """
+    # 打开图片
+    img = Image.open(image_path)
+
+    # 获取图片的原始宽度和高度
+    original_width, original_height = img.size
+
+    # 计算原始图像的宽高比
+    aspect_ratio = original_width / original_height
+
+    scaled_height = int(target_width / aspect_ratio)
+
+    # 图片半高
+    half_height = int(scaled_height // 2)
+
+    # 缩放图片
+    img = img.resize((target_width, scaled_height))
+
+    # 创建一个新的RGBA图像，大小为目标显示区域的尺寸,作为处理图片再提
+    new_img = Image.new("RGBA", (target_width, target_height))
+
+    # 上图载体
+    up_img = Image.new("RGBA", (target_width, scaled_height))
+    up_img.paste(img, (0, 0))
+
+    # 计算上部分裁剪区域的左上角和右下角坐标
+    up_img = up_img.crop((0, 0, target_width, half_height))
+    new_img.paste(up_img, (0, 0))
+
+    # 下图载体
+    down_img = Image.new("RGBA", (target_width, scaled_height))
+    down_img.paste(img, (0, 0))
+
+    # 计算下部分裁剪区域的左上角和右下角坐标
+    down_img = down_img.crop(
+        (0, half_height, target_width, scaled_height))
+    new_img.paste(down_img, (0, int(target_height - half_height)))
+
+    # 中图载体
+    center_img = Image.new("RGBA", (target_width, scaled_height))
+    center_img.paste(img, (0, 0))
+
+    # 计算中部分裁剪区域的左上角和右下角坐标
+    lower_center = half_height + 50
+    center_img = center_img.crop((0, half_height, target_width, lower_center))
+
+    new_center_img = center_img.resize(
+        (target_width, target_height - scaled_height))
+
+    new_img.paste(new_center_img, (0, half_height))
+
+    return new_img
+
+
+def header_text(text_str, image, textCount=None, top_text='TOP', top_title="榜单"):
+    """
+    绘制头部文字
+    :param text: 文字
+    :param image: 图片
+    :return: 绘制后的图片
+    """
+    # 创建一个用于绘制的Draw对象
+    draw = ImageDraw.Draw(image)
+
+    # 创建一个红色的方形圆角框
+    # 宽是 500  ，高是 100，居中显示
+    box_width = 500
+    margin = 80
+    top = 40
+    height = 100
+
+    # 偏移 10 ，白色底层
+    draw.rounded_rectangle(
+        [(margin + 10, top - 10), (box_width - margin + 10, height + top - 10)],
+        fill='#fff',
+        outline='#4f562d',
+        radius=10
+    )
+
+    # 偏移 10 ，绿色底层
+    draw.rounded_rectangle(
+        [(margin - 10, top + 40), (box_width - margin - 10, height + top + 40)],
+        fill='#50b674',
+        outline='#4f562d',
+        radius=10
+    )
+
+    # 黄色正文
+    draw.rounded_rectangle(
+        [(margin, top), (box_width - margin, height + top)],
+        fill='#f3d960',
+        outline='#4f562d',
+        radius=10
+    )
+
+    # 字体行距
+    lines_space = 12
+
+
+    content_font = ImageFont.truetype(font_path, 24)
+    content_width, _ = content_font.getsize(text_str)
+    left_margin = (box_width - content_width) / 2
+
+    draw.text(xy=(left_margin, height + top + 8), text=text_str,
+              fill="#333", font=content_font, spacing=lines_space)
+    draw.text(xy=(left_margin + 1, height + top + 7), text=text_str,
+              fill="#fff", font=content_font, spacing=lines_space)
+
+    top_font = ImageFont.truetype(font_path, 92)
+    x = margin + 10
+    y = top + 10
+
+    text = top_text
+    text_width, text_height = top_font.getsize(text)
+
+    text_img = Image.new(
+        "RGBA", (text_width, text_height + 10), (255, 255, 255, 0))
+    text_draw = ImageDraw.Draw(text_img)
+
+    # 制作阴影效果
+    # 描边效果
+    text_draw.text(xy=(0, 3), text=text, fill="#f4362a",
+                   font=top_font, spacing=lines_space)
+    # 文字旋转
+
+    text_draw.text(xy=(3, 0), text=text, fill="#fff",
+                   font=top_font, spacing=lines_space)
+    # 文字旋转
+    rotated_text_img = text_img.rotate(10, expand=1)
+
+    # 计算旋转后的文字应该被粘贴的位置
+    paste_x = x + (text_width - rotated_text_img.width) / 2
+    paste_y = y + (text_height - rotated_text_img.height) / 2
+    image.paste(rotated_text_img, (int(paste_x),
+                                   int(paste_y)), rotated_text_img)
+    if textCount:
+        text = '抽奖'
+    else:
+        text = top_title
+    bd_font = ImageFont.truetype(font_path, 78)
+    bd_text_width, bd_text_height = top_font.getsize(text)
+
+    bg_text_img = Image.new(
+        "RGBA", (bd_text_width, bd_text_height + 10), (255, 255, 255, 0))
+    bg_text_draw = ImageDraw.Draw(bg_text_img)
+    bg_text_draw.text(xy=(0, 3), text=text, fill="#fff",
+                      font=bd_font, spacing=lines_space)
+    bg_text_draw.text(xy=(3, 0), text=text, fill="#000",
+                      font=bd_font, spacing=lines_space)
+
+    image.paste(bg_text_img, (int(text_width + x + 15), int(y)), bg_text_img)
+
+    # 保存图像
+    return draw
+
+
+def content_text(data, index, height=40):
+    """
+    绘制内容文字
+    :param data: 数据
+    :param image: 图片
+    :return: 绘制后的图片
+    """
+    username = data.get('username', '-')
+    # 获取总数
+    floor_num = data.get('floor_num', '-')
+
+    # 创建一个用于绘制的Draw对象
+    width = 500
+    margin = 80
+    # 获取1的path
+    bj1_path = text_bg_path / f'jp1.png'
+    img_path = text_bg_path / f'jp0.png'
+
+    # 打开图片
+    img = Image.open(img_path)
+    # 将图片转换为"RGBA"模式
+    img = img.convert("RGBA")
+
+    box_img = Image.open(bj1_path)
+
+    box_original_width, box_original_height = box_img.size
+    # 计算奖牌原始图像的宽高比
+    box_aspect_ratio = box_original_width / box_original_height
+    box_height = height
+    box_width = int(box_height * box_aspect_ratio)
+
+    # 获取图片的原始宽度和高度
+    original_width, original_height = img.size
+
+    # 计算原始图像的宽高比
+    aspect_ratio = original_width / original_height
+    img_height = height
+
+    img_width = int(img_height * aspect_ratio)
+
+    img = img.resize((img_width, img_height))
+
+    # 创建大小
+    item_img = Image.new(
+        "RGBA", (width - margin * 2, height + 10), (255, 255, 255, 0))
+
+    header_img = Image.new("RGBA", (box_width, box_height), (255, 255, 255, 0))
+
+    x = (box_width - img_width) // 2
+    y = (box_height - img_height) // 2
+    header_img.paste(img, (x, y), img)
+
+    item_img.paste(header_img, (0, 0))
+
+    # 创建一个用于绘制的Draw对象
+    draw = ImageDraw.Draw(item_img)
+
+    # # 字体行距
+    lines_space = 12
+
+    size =17
+    title_text = ImageFont.truetype(font_path, size)
+
+    text = username
+    # if index == 0:
+    #     text = '[逼话王] ' + text
+    sub_text = f'楼层： {floor_num} 楼'
+
+    # 大于 13 个汉子，显示...
+    if len(text) > 13:
+        text = text[:13] + '...'
+    if len(sub_text) > 19:
+        sub_text = sub_text[:19] + '...'
+
+    title_color = '#000' if index < 3 else '#666'
+
+    draw.text(xy=(box_width + 10, 5), text=text, fill=title_color,
+              font=title_text, spacing=lines_space)
+    title_text = ImageFont.truetype(font_path, 15)
+    draw.text(xy=(box_width + 10, 27), text=sub_text, fill="#666",
+              font=title_text, spacing=lines_space)
+
+    # 排行数字
+    rank_text = ImageFont.truetype(font_path, 14)
+
+    font_width, font_height = rank_text.getsize(str(index + 1))
+    font_x = (box_width - font_width) // 2
+    font_y = (box_height - font_height) // 2
+    draw.text(xy=(font_x, font_y - 3), text=str(index + 1), fill="#fff",
+              font=rank_text, spacing=lines_space)
+
+    return item_img
+
+
+class TxtToImg:
+    def __init__(self) -> None:
+        pass
+
+    def run(
+            self,
+            data={},
+            date_str='',
+            textCount=None,
+            top_text='TOP',
+            top_title="榜单",
+            title=""
+    ) -> bytes:
+        """将文本转换为图片"""
+        bg_image_path = text_bg_path / f'bg-1.jpg'
+        font = ImageFont.truetype(font_path, 14)
+
+        lines_space = 8
+        img_width = 500
+        # 最小基础高度，12条数据
+        img_height = 870
+
+        # 迭代次数
+        lice = 15
+
+        # 活跃用户查询
+        if top_text == '黑盒':
+            lice = len(data)
+            title += f'，共 {lice} 人'
+
+        # 渲染列表的内容高度
+        content_top = 192
+        # 间距
+        margin = 80
+        # 单条内容高度
+        content_height = 40
+        margin_height = content_height + 10
+
+        if lice > 12:
+            lines_height = lice - 12
+            img_height += (lines_height * margin_height)
+
+        # 这是不要背景 ，纯文字
+        image = Image.new("RGBA", (img_width, img_height), '#ffffff')
+
+        background_image_path = Path("resource") / bg_image_path
+
+        # 创建一个新的RGBA图像，大小为目标显示区域的尺寸
+        output_image = Image.new(
+            "RGBA", (img_width, img_height), (255, 255, 255, 0))
+
+        bg_img = adjust_image(background_image_path, img_width, img_height)
+
+        # 将调整后的图片粘贴到输出图像中，以居中显示
+        output_image.paste(bg_img, (0, 0))
+
+        for index, item in islice(enumerate(data), lice):
+            content_img = content_text(item, index, content_height)
+            output_image.paste(content_img, (margin, content_top), content_img)
+            content_top += margin_height
+
+        if len(data) < lice or lice == 0:
+            draw = ImageDraw.Draw(output_image)
+            text = '--- 虚位以待 ---'
+            font_width, _ = font.getsize(text)
+            draw.text(xy=((img_width - font_width) // 2, content_top + 20), text=text, fill="#999",
+                      font=font, spacing=lines_space)
+
+        image.paste(output_image, (0, 0))
+
+        header_text(text_str=title, image=image, textCount=textCount,
+                    top_text=top_text, top_title=top_title)
+
+        img_byte = BytesIO()
+        image.save(img_byte, format="PNG")
+
+        return True, img_byte
+
+
+def xhh2img(data={}, date_str='', textCount=None, top_text='TOP', top_title="榜单", title=""):
+    xhhImg = TxtToImg()
+
+    return xhhImg.run(
+        data=data,
+        date_str=date_str,
+        textCount=textCount,
+        top_text=top_text,
+        top_title=top_title,
+        title=title
+    )
--- src/plugins/nonebot_plugin_group_master/xhh_admin.py
+++ src/plugins/nonebot_plugin_group_master/xhh_admin.py
@@ -16,9 +16,9 @@ from nonebot.params import EventPlainText
 from nonebot.plugin import PluginMetadata
 from nonebot.typing import T_State
 
-from .serivce.xhh_source import bind_user
+from .serivce.xhh_source import bind_user, get_article_info, query_user, get_detail_info
 from .utils import At, MsgText, Reply, check_func_status, check_message_legality
-from .utils.xhh import get_article_list, get_user_info
+from .utils.xhh import get_user_info
 
 
 def match_url_with_link_id(url_pattern: str) -> Callable[[str], bool]:
@@ -28,6 +28,7 @@ def match_url_with_link_id(url_pattern: str) -> Callable[[str], bool]:
             query_params = parse_qs(parsed_url.query)
             return "link_id" in query_params and message.startswith(url_pattern.split("?")[0])
         return False
+
     return rule
 
 
@@ -35,7 +36,6 @@ look_answer = on_message(rule=match_url_with_link_id(
     "https://api.xiaoheihe.cn/v3/bbs/app/api/web/share"))
 
 share_card = on_message(priority=11, block=False)
-
 
 xhh_aliases = ['小黑盒', 'xhh', '黑盒']
 xhh = on_command('小黑盒', aliases=set(
@@ -60,13 +60,13 @@ async def xhh_handle(bot: Bot, matcher: Matcher, event: GroupMessageEvent):
     # 一个参数直接查询信息
     if len(params) == 1:
         await bot.send(event, "小黑盒信息查询中，请稍后")
-        # msg = await query_steam_user(user_id=uid)
+        msg = await query_user(user_id=uid)
         return await bot.send(event=event, message=msg)
 
     # 指令
     command = params[1]
     # 合法指令
-    command_list = ['绑定', '查询', '更新', 'help', '帮助']
+    command_list = ['绑定', '查询', '更新', 'help', '帮助', '开奖']
     if command not in command_list:
         # 分享链接  https://api.xiaoheihe.cn/v3/bbs/app/api/web/share?link_id=133815114
         # 是url 并且有link_id
@@ -76,7 +76,8 @@ async def xhh_handle(bot: Bot, matcher: Matcher, event: GroupMessageEvent):
             query_params = parse_qs(parsed_url.query)
             link_id = query_params.get("link_id", [None])[0]
             if link_id:
-                await bot.send(event, f"Link ID: {link_id}")
+                msg = await get_article_info(link_id=link_id, user_id=uid)
+                return await matcher.send(MessageSegment.at(uid) + '\n' + msg)
             else:
                 return await bot.send(event, "URL 错误，请直接从小黑盒分享中复制链接")
         else:
@@ -97,39 +98,70 @@ async def xhh_handle(bot: Bot, matcher: Matcher, event: GroupMessageEvent):
     # 查询
     if command == '绑定':
         xid = params[2]
-        # return await bot.send(event, "查询")
-
-        # steamid = get_steam_id(params[2])
-
-        # print('steamid', steamid)
-
-        # if not steamid:
-        #     return await bot.send(event=event, message=MessageSegment.at(uid) + " Steam 好友代码错误")
-
-        # msg = await bind_user(group_id=gid, user_id=uid, sender=sender, xid=params[2])
         user_res = get_user_info(user_id=xid)
         if user_res is None:
             return await bot.send(event, "未找用户信息,请检查小黑盒ID是否正确")
-        # try:
-                
-        await bind_user(group_id=gid, user_id=uid, sender=sender, xid=params[2])
+        try:
 
-        print("res:", user_res)
-        msg = MessageSegment.at(uid) + (
-            f"绑定成功：",
-            f"\n小黑盒ID：{xid} \n" if xid else f"\n",
-            f"昵称：{user_res.get('username',f'用户{xid}')}\n",
-            f"等级：{user_res.get('level_info',{}).get('level','-')}\n",
-        )
+            forbid_info = user_res.get('forbid_info', '')
+            if forbid_info:
+                text = f"绑定失败：{forbid_info}\n"
 
-        await bot.send(event=event, message=msg)
-        # except Exception as e:
-        #     logger.error(f"绑定小黑盒ID失败: {e}")
-        #     return await bot.send(event, "绑定失败，请稍后再试！")
+                return await bot.send(event, MessageSegment.at(uid) + text)
+
+            await bind_user(group_id=gid, user_id=uid, sender=sender, xid=params[2])
+
+            msg_text = (
+                f"绑定成功：",
+                f"\n小黑盒ID：{xid} \n" if xid else f"\n",
+                f"昵称：{user_res.get('username', f'用户{xid}')}\n",
+                f"等级：{user_res.get('level_info', {}).get('level', '-')}\n",
+            )
+
+            msg = MessageSegment.at(uid) + Message(msg_text)
+
+            await bot.send(event=event, message=msg)
+        except Exception as e:
+            logger.error(f"绑定小黑盒ID失败: {e}")
+            return await bot.send(event, "绑定失败，请稍后再试！")
+
+    if command == '开奖':
+        links = ''
+        link_data_id = ''
+        num = 1
+        if len(params) == 2:
+            return await bot.send(event, "请输入开奖链接,从文章分享里复制！")
+        if len(params) == 3:
+            links = params[2]
+
+        if len(params) == 4:
+            links = params[2]
+            num = params[3]
+            if not num.isdigit():
+                return await bot.send(event, "请输入正确的数字")
+            num = int(num)
+            if num == 0:
+                return await bot.send(event, "开奖最少一个人")
+
+        if links.startswith("https://api.xiaoheihe.cn/v3/bbs/app/api/web/share"):
+            parsed_url = urlparse(links)
+            query_params = parse_qs(parsed_url.query)
+            link_id = query_params.get("link_id", [None])[0]
+            if link_id:
+                link_data_id = link_id
+            else:
+                return await bot.send(event, "URL 错误，请直接从小黑盒分享中复制链接")
+        else:
+            return await bot.send(event, f"URL 错误，请直接从小黑盒分享中复制链接")
+
+        await get_detail_info(link_id=link_data_id, user_id=uid, matcher=matcher, event=event, bot=bot, num=num)
+
+        # 返回message 发送信息 ，返回数组 进行处理
+
 
 @look_answer.handle()
 async def answersbook(
-    bot: Bot, event: MessageEvent, state: T_State, message: str = EventPlainText()
+        bot: Bot, event: MessageEvent, state: T_State, message: str = EventPlainText()
 ) -> None:
     state["user_id"] = event.user_id
     parsed_url = urlparse(message)
@@ -147,6 +179,7 @@ async def answersbook(
 async def share_card_handle(bot: Bot, matcher: Matcher, event: GroupMessageEvent) -> None:
     try:
         data = json.loads(event.json())
+        user_id = str(event.user_id)
         for msg in data["message"]:
             if msg["type"] == "json" and msg["data"]:
                 msgdata = json.loads(msg["data"]["data"])
@@ -157,71 +190,8 @@ async def share_card_handle(bot: Bot, matcher: Matcher, event: GroupMessageEvent
                     link_id = parse_qs(parsed_url.query).get(
                         "link_id", [None])[0]
                     if link_id:
-                        await matcher.send(f"Link ID: {link_id}")
-                        return
+                        msg = await get_article_info(link_id=link_id, user_id=user_id, c_type=True)
+                        if msg:
+                            return await matcher.send(msg, reply_message=True)
     except Exception as e:
         logger.error(f"处理分享卡片时出错: {e}")
-    await matcher.send("这是一张分享卡片！")
-
-# # 获取小黑盒文章信息
-# def startswith_or_endswith(msg: str) -> Callable[[str], bool]:
-#     def rule(message: str = EventPlainText()) -> bool:
-#         return message.startswith(msg) or message.endswith(msg)
-#     return rule
-
-# look_answer = on_message(rule=startswith_or_endswith("黑盒文章"))
-
-
-# @look_answer.handle()
-# async def answersbook(
-#     event: MessageEvent, state: T_State, message: str = EventPlainText()
-# ) -> None:
-#     state["user_id"] = event.user_id
-#     if event.reply or message.strip() != "黑盒文章":
-#         state["question"] = True
-
-
-# async def question(bot:Bot ,event: MessageEvent) -> None:
-#     pass
-
-# @look_answer.got("question", prompt=Message.template("{user_id:at} 请输入小黑盒文章或图文的URL（文章点击分享，点击复制链接）。"))
-# async def question(bot:Bot ,event: MessageEvent) -> None:
-
-#     # 是否回复
-#     rp = Reply(event.json())
-
-#     if rp:
-#         words = rp['message']
-#         print("words:",words)
-#         if len(words) == 1:
-#             wrod = words[0]
-#             if wrod['type'] == 'text':
-#                 text = wrod['data']['text']
-#                 text = text.strip()
-#                 try:
-#                     # 解析 URL
-#                     parsed_url = urlparse(text)
-#                     query_params = parse_qs(parsed_url.query)
-
-#                     # 获取 link_id 参数
-#                     link_id = query_params.get("link_id")
-#                     if link_id:
-#                         link_id = link_id[0]  # 获取第一个值
-#                         return await bot.send(event,f"Link ID: {link_id}")
-#                     else:
-#                         return await look_answer.reject("URL 中没有找到 link_id 参数！")
-#                 except Exception as e:
-#                     return await look_answer.reject(f"解析 URL 时出错: {e}")
-
-#             return await look_answer.reject("回复格式错误，只能回复链接地址！")
-#         return await look_answer.reject("回复信息错误，只能回复自己发布的小黑盒链接地址！")
-
-#     reply_id = event.reply.message_id if event.reply else event.message_id
-#     # 获取消息内容
-#     text = event.message.extract_plain_text().strip()
-#     print("text:",text)
-#     await look_answer.finish(
-#         MessageSegment.reply(reply_id)
-#         + MessageSegment.at(event.user_id)
-#         + MessageSegment.text('test')
-#     )
